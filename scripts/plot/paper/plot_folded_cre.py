6c43a3e2147e047b858be9ec135133f5d365aeda
jakob.deutloff <jakob.deutloff@gmail.com>
2024-08-21 14:04:27 +0200

    ARTS CRE added to folded CRE plot

--- scripts/plot/paper/plot_folded_cre.py
+++ scripts/plot/paper/plot_folded_cre.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 import xarray as xr
 import numpy as np
 import pickle
+from src.read_data import load_cre
 
 # %% load data
 ds_monsoon = xr.open_dataset("/work/bm1183/m301049/iwp_framework/mons/data/full_snapshot_proc.nc")
+cre, cre_mean = load_cre()
 path = "/work/bm1183/m301049/iwp_framework/mons/model_output/"
 run = "frozen_only"
 with open(path + run + ".pkl", "rb") as f:
@@ -21,6 +23,9 @@ hist = hist / n_profiles
 cre_sw_weighted = hist * result['SW_cre']
 cre_lw_weighted = hist * result['LW_cre']
 cre_net_weighted = cre_sw_weighted + cre_lw_weighted
+cre_arts_sw_weighted = hist * cre_mean['connected_sw']
+cre_arts_lw_weighted = hist * cre_mean['connected_lw']
+cre_arts_net_weighted = cre_arts_sw_weighted + cre_arts_lw_weighted
 
 # %% plot all three in one figure
 fig, axes = plt.subplots(3, 1, figsize=(10, 6), height_ratios=[1, 1, 2], sharex=True)
@@ -85,5 +90,29 @@ ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
 ax.legend()
 fig.savefig("plots/paper/concept_cre.png", dpi=500, bbox_inches='tight')
+
+# %% plot for presentation with ARTS 
+
+fig, ax = plt.subplots(figsize=(7, 3))
+
+# plot cre weighted IWP hist
+ax.stairs(cre_sw_weighted, IWP_bins, color='blue', label='SW')
+ax.stairs(cre_lw_weighted, IWP_bins, color='red', label='LW')
+ax.stairs(cre_net_weighted, IWP_bins, color='k', fill=True, alpha=0.5)
+ax.stairs(cre_net_weighted, IWP_bins, color='k', label='Net')
+
+# plot arts as well (for presentation)
+ax.stairs(cre_arts_sw_weighted, IWP_bins, color='blue', linestyle='--', label='SW (arts)')
+ax.stairs(cre_arts_lw_weighted, IWP_bins, color='red', linestyle='--', label='LW (arts)')
+ax.stairs(cre_arts_net_weighted, IWP_bins, color='k', linestyle='--', label='Net (arts)')
+
+ax.set_xscale('log')
+ax.set_xlabel('Ice Water Path / kg m$^{-2}$')
+ax.set_ylabel(r"$\mathrm{HCRE} \cdot P ~ / ~ \mathrm{W ~ m^{-2}}$")
+ax.set_yticks([-1, 0, 1])
+ax.set_xlim(1e-5, 10)
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+fig.savefig("plots/presentation/concept_cre.png", dpi=500, bbox_inches='tight')
 
 # %%
