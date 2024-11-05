c3841793e388d7bed83c0aad1020dd06df926296
jakob.deutloff <jakob.deutloff@gmail.com>
2024-11-05 12:57:33 +0100

    processing of nosnow fluxes done

--- scripts/model_runs/run_whole_analysis.sh
+++ scripts/model_runs/run_whole_analysis.sh
@@ -4,9 +4,10 @@
 export PYTHONPATH="${PYTHONPATH}:/home/m/m301049/HcModel/"
 
 # execute python script in respective environment 
-/home/m/m301049/miniconda3/envs/defaultenv/bin/python /home/m/m301049/HcModel/scripts/process_data/process_atms_and_fluxes_mons.py
-/home/m/m301049/miniconda3/envs/defaultenv/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_cre.py
-/home/m/m301049/miniconda3/envs/defaultenv/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_emissivity.py
-/home/m/m301049/miniconda3/envs/defaultenv/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_albedo.py
-/home/m/m301049/miniconda3/envs/defaultenv/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_albedo_t_and_R_t.py
-/home/m/m301049/miniconda3/envs/defaultenv/bin/python /home/m/m301049/HcModel/scripts/model_runs/icon_mons.py
+/home/m/m301049/.conda/envs/main/bin/python /home/m/m301049/HcModel/scripts/process_data/process_atms_and_fluxes_mons.py
+/home/m/m301049/.conda/envs/main/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_cre.py
+/home/m/m301049/.conda/envs/main/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_emissivity.py
+/home/m/m301049/.conda/envs/main/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_albedo.py
+/home/m/m301049/.conda/envs/main/bin/python /home/m/m301049/HcModel/scripts/derive_model_relations/calculate_albedo_t_and_R_t.py
+/home/m/m301049/.conda/envs/main/bin/python /home/m/m301049/HcModel/scripts/model_runs/icon_mons.py
+
